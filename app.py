e47f7fad4f343242e06639938f4535b0a754000f
Renan Januário <renanjanuario@protonmail.com>
2025-01-10 15:16:48 -0300

    atual

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ app.secret_key = os.getenv('SECRET_KEY', 'chave_padrao_segura')
 
 # Configuração do banco de dados
 
-# Corrigir o prefixo postgres:// para postgresql://
+# Corrigir o prefixo da DATABASE_URL
 database_url = os.getenv('DATABASE_URL')
 if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///igreja.db')  # Fallback para SQLite
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
